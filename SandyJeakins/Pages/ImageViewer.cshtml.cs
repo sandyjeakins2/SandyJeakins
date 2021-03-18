c15036080015227ca1bc6240ba925529dbe35649
Sandy Jeakins <sjeakins@marathonus.com>
2021-03-18 15:07:43 -0400

    Initial Revision

---
+++ SandyJeakins/Pages/ImageViewer.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SandyJeakins
+{
+    public class ImageViewerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
